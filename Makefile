4714d4399bf9de8cf744c35dd2a6accdfa7ba6ef
Stabbed-wound <github.com.easter342@passmail.net>
2025-02-23 10:14:22 +1030

    changed architecture of libft

--- Makefile
+++ Makefile
@@ -1,57 +1,46 @@
-NAME		= ft
+NAME			:= libft.a
 
-SRC_DIR		= srcs
-SRCS		+= $(addprefix $(SRC_DIR)/binary_tree/ft_,$(addsuffix .c, balance_factor balance clear height insert_balanced insert is_balanced new rotate_left rotate_right search size))
-SRCS		+= $(addprefix $(SRC_DIR)/char/ft_,$(addsuffix .c, isalnum isalpha isascii isdigit isprint isspace tolower toupper))
-SRCS		+= $(addprefix $(SRC_DIR)/converters/ft_,$(addsuffix .c, atod atoi atol itoa_base itoa uitoa_base uitoa ultoa_base))
-SRCS		+= $(addprefix $(SRC_DIR)/doubly_linked_list/ft_,$(addsuffix .c, add_back add_front clear cmp find first index insert_after insert_before is_sorted iter last match new remove_elem remove size))
-SRCS		+= $(addprefix $(SRC_DIR)/linked_list/ft_lst,$(addsuffix .c, add_back add_front clear find index is_sorted iter last map max min new pop remove size split_before split))
-SRCS		+= $(addprefix $(SRC_DIR)/memory/ft_,$(addsuffix .c, bzero calloc memchr memcmp memcpy memmove memset))
-SRCS		+= $(addprefix $(SRC_DIR)/stack/ft_,$(addsuffix .c, clear new_from_ints new_from_list new pop push rotate_rev rotate swap_top))
-SRCS		+= $(addprefix $(SRC_DIR)/string/ft_,$(addsuffix .c, ft_split strchr strchrs strcmp strdup striteri strjoin strlcat strlcpy strlen strmapi strncmp strndup strnstr strrchr strtoupper strtrim substr))
+export ROOT		:= $(shell pwd)
 
-BUILD_DIR	= .build
-OBJ_DIR		= $(BUILD_DIR)/objs
-OBJS		= $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%.o,$(SRCS))
-DEP_DIR		= $(BUILD_DIR)/deps
-DEPS		= $(patsubst $(SRC_DIR)/%.c,$(DEP_DIR)/%.d,$(SRCS))
+BUILD_DIR		:= $(ROOT)/.build
+OBJ_DIR			:= $(BUILD_DIR)/objs
+DEP_DIR			:= $(BUILD_DIR)/deps
 
-CC			?= cc
-CFLAGS		?= -Wall -Werror -Wextra
-CPPFLAGS	:= -MMD -MP
+COMPONENTS		:= \
+	binary_tree char converters doubly_linked_list \
+	linked_list memory string
+
+export CC		?= cc
+export CFLAGS	?= -Wall -Werror -Wextra
 
 AR			= ar
 ARFLAGS		= -rcs
 
-.PHONY: all clean fclean re norm
-.SILENT:
-
 all: $(NAME)
 
-$(NAME): $(OBJS)
-	$(AR) $(ARFLAGS) $@ $^
-	$(info CREATED $@)
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
-	mkdir -p $(@D)
-	mkdir -p $(dir $(DEP_DIR)/$*.d)
-	$(CC) $(CFLAGS) $(CPPFLAGS) -c $^ -o $@
-	$(info CREATED $@)
-
--include $(DEPS)
+$(NAME):
+	for f in $(COMPONENTS); do $(MAKE) -C $$f; done
+	$(AR) $(ARFLAGS) $@ $(wildcard $(OBJ_DIR))
+	echo CREATED $@
 
 clean:
-	$(info Cleaning libft...)
-	rm -fv $(OBJS) $(DEPS)
+	echo Cleaning libft...
+	for f in $(COMPONENTS); $(MAKE) -C $$f clean; done
 	rm -rf $(BUILD_DIR)
-	$(info Done)
+	echo Done
 
 fclean:
-	$(info Fully cleaning libft...)
-	rm -fv $(OBJS) $(DEPS) $(NAME)
+	echo Fully cleaning libft...
+	for f in $(COMPONENTS); $(MAKE) -C $$f clean; done
+	echo REMOVED $(NAME)
 	rm -rf $(BUILD_DIR)
-	$(info Done)
+	echo Done
 
-re: fclean all
+re:
+	$(MAKE) fclean
+	$(MAKE) all
 
 norm: ; norminette | grep -v OK
+
+.PHONY: all clean fclean re norm
+.SILENT:
--- libft.h
+++ libft.h
@@ -1,12 +1,131 @@
 #ifndef LIBFT_H
 # define LIBFT_H
-# include "headers/ft_binary_tree.h"
-# include "headers/ft_char.h"
-# include "headers/ft_converters.h"
-# include "headers/ft_doubly_linked_list.h"
-# include "headers/ft_linked_list.h"
-# include "headers/ft_memory.h"
-# include "headers/ft_stack.h"
-# include "headers/ft_string.h"
+# include <stdlib.h>
+
+// binary_tree
+typedef struct s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	size_t			key;
+	void			*value;
+}	t_btree;
+
+t_btree	*ft_btreenew(size_t key, void *value);
+t_btree	*ft_btreesearch(t_btree *root, size_t key);
+void	ft_btreeinsert(t_btree **rootptr, t_btree *new);
+void	ft_btreeinsert_balanced(t_btree **rootptr, t_btree *new);
+void	ft_btreeclear(t_btree **rootptr, void (*fr)(void *));
+void	ft_btreerotate_left(t_btree **rootptr);
+void	ft_btreerotate_right(t_btree **rootptr);
+void	ft_btreebalance(t_btree **rootptr);
+size_t	ft_btreesize(t_btree *root);
+size_t	ft_btreeheight(t_btree *root);
+int		ft_btreeis_balanced(t_btree *root);
+int		ft_btreebalance_factor(t_btree *root);
+
+// char
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isspace(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+// converters
+int		ft_atoi(const char *s);
+long	ft_atol(const char *s);
+double	ft_atod(const char *s);
+char	*ft_itoa(int n);
+char	*ft_itoa_base(int n, char *base);
+char	*ft_uitoa(unsigned int n);
+char	*ft_uitoa_base(unsigned int n, char *base);
+char	*ft_ultoa_base(unsigned long n, char *base);
+
+// doubly_linked_list
+typedef struct s_dllist
+{
+	struct s_dllist	*prev;
+	struct s_dllist	*next;
+	void			*data;
+}	t_dllist;
+
+t_dllist	*ft_dllstnew(void *data);
+t_dllist	*ft_dllstlast(t_dllist *head);
+t_dllist	*ft_dllstfirst(t_dllist *elem);
+t_dllist	*ft_dllstindex(t_dllist *head, size_t index);
+t_dllist	*ft_dllstfind(t_dllist *head, void *ref,
+				int (*cmp)(void *, void *));
+void		ft_dllstadd_front(t_dllist **headptr, t_dllist *new);
+void		ft_dllstadd_back(t_dllist **headptr, t_dllist *new);
+void		ft_dllstinsert_before(t_dllist *elem, t_dllist *new);
+void		ft_dllstinsert_after(t_dllist *elem, t_dllist *new);
+void		ft_dllstremove(t_dllist **headptr, size_t index,
+				void (*free)(void *));
+void		ft_dllstremove_elem(t_dllist **elemptr, void (*fr)(void *));
+void		ft_dllstclear(t_dllist **headptr, void (*free)(void *));
+void		ft_dllstiter(t_dllist *head, void (*f)(void *));
+size_t		ft_dllstsize(t_dllist *head);
+int			ft_dllstis_sorted(t_dllist *head, int (*cmp)(void *, void *));
+int			ft_dllstcmp(t_dllist *l1, t_dllist *l2, int (*cmp)(void *, void *));
+int			ft_dllstmatch(t_dllist *find, t_dllist *in,
+				int (*cmp)(void *, void *));
+
+// linked_list
+typedef struct s_list
+{
+	void			*data;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *data);
+t_list	*ft_lstlast(t_list *head);
+t_list	*ft_lstindex(t_list *head, size_t index);
+t_list	*ft_lstfind(t_list *head, void *ref, int (*cmp)(void *, void *));
+t_list	*ft_lstmap(t_list *head, void *(*f)(void *));
+t_list	*ft_lstsplit(t_list **headptr, int (*pred)(void *),
+			void (*f_free)(void *)); // Deletes split points
+t_list	*ft_lstsplit_before(t_list **headptr, int (*pred)(void *));
+t_list	*ft_lstpop(t_list **headptr, size_t index);
+void	ft_lstadd_front(t_list **headptr, t_list *new);
+void	ft_lstadd_back(t_list **headptr, t_list *new);
+void	ft_lstremove(t_list **headptr, size_t index, void (*f_free)(void *));
+void	ft_lstclear(t_list **headptr, void (*f_free)(void *));
+void	ft_lstiter(t_list *head, void (*f)(void *));
+size_t	ft_lstmin(t_list *head, int (*cmp)(void *, void *));
+size_t	ft_lstmax(t_list *head, int (*cmp)(void *, void *));
+size_t	ft_lstsize(t_list *head);
+int		ft_lstis_sorted(t_list *head, int (*cmp)(void *, void *));
+
+// memory
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *p1, const void *p2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+
+// string
+void	ft_strtoupper(char *s);
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strchrs(const char *s, const char *cs);
+char	*ft_strdup(const char *s);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strndup(const char *s, size_t n);
+char	*ft_strnstr(const char *h, const char *n, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
