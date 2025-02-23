94a03ebf3323999eb1abbb971b3dab94accd6352
Stabbed-wound <github.com.easter342@passmail.net>
2025-02-23 19:07:18 +1030

    Added free

--- allocator/srcs/ft_free.c
+++ allocator/srcs/ft_free.c
@@ -1,8 +1,25 @@
 #include "allocator.h"
 
-// TODO
+int	cmp_ptrs(void *vchunk, void *ptr)
+{
+	return (((t_chunk *)vchunk)->start != ptr);
+}
+
 void	ft_free(void *ptr)
 {
+	t_heap		*heap;
+	t_dllist	*chunks;
+
 	if (!ptr)
 		return ;
+	heap = get_heap();
+	chunks = ft_dllstfind(heap->meta, ptr, cmp_ptrs);
+	if (!chunks || ((t_chunk *)chunks->data)->is_free)
+		return ;
+	ft_dllstremove_elem(&chunks, free);
+	if (((t_chunk *)chunks->data)->is_free && ((t_chunk *)chunks->prev->data)->is_free)
+	{
+		((t_chunk *)chunks->prev->data)->size += ((t_chunk *)chunks->data)->size;
+		ft_dllstremove_elem(&chunks, free);
+	}
 }
